303fa2cd7b9327dea9c20dcb6b33afe00cbc9d01
bismar <manganelly@yahoo.es>
2023-03-29 15:29:19 -0400

    skeleton server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express'); //import express from'express'//
+
+
+
+const app = express();
+
+const port = 9000;
+
+
+app.get('/', (req, res) => {
+
+  res.send("hello world");
+
+});
+
+
+
+app.listen(port, () => console.log(`App running on port:  ${port}`));
